7acbba533a5907c9a58c89ed5035a35f5a2751df
Kevin McDonnell <kevin@mcd79.com>
2023-01-24 22:10:26 +0000

    Removed contentful stuff

--- sources/contentful/modelExtensions/Config.ts
+++
@@ -1,8 +0,0 @@
-import { ModelExtension } from '@stackbit/types';
-
-export const Config: ModelExtension = {
-    name: 'Config',
-    label: 'Config',
-    singleInstance: true,
-    readOnly: true
-};
